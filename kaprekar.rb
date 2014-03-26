d065af89eedb8ce3eaf57eeaf27bfdfee4be5e22
Taha Jalil <taha.jalil@gmail.com>
2014-03-25 21:15:09 -0400

    converted k.to_s

--- kaprekar.rb
+++ kaprekar.rb
@@ -1,3 +1,15 @@
+=begin
+9 is a Kaprekar number since 
+9 ^ 2 = 81 and 8 + 1 = 9
+
+297 is also Kaprekar number since 
+297 ^ 2 = 88209 and 88 + 209 = 297.
+
+In short, for a Kaprekar number k with n-digits, if you square it and add the right n digits to the left n or n-1 digits, the resultant sum is k. 
+=end
+
 def kaprekar?(k)
-	
+	n = k.to_s.length
 end
+
+puts kaprekar?(8453)
